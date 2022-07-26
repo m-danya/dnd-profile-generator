c6804c4b6473cf167eaf0f156eec30c3c9d34006
m-danya <danila-mikh@ya.ru>
2022-07-26 23:48:23 +0300

    Add text + round avatar's corners
    Also change the printout size to 2480x3508 hard-coded, which corresponds
    A4 at 300dpi. This hack solves font sizes problem: if the image size is
    not fixed, the font size will differ from image to image and the only
    solution to get a text label of the same size is to brute-force font
    sizes, which is not cool. So this hack is better. Also, it guarantees
    the correctness of an output size since it does not depend on the
    background size anymore.
    
    Note: the printout result is served incorrectly right now, IDK why.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Create a beautiful printout for your DnD character
 
 #### Development status
 
-45% is done
+65% is done
 
 
 ### How to host
--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from base64 import decode
 from werkzeug.utils import secure_filename
 from pathlib import Path
 import uuid
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 import json
 
 UPLOAD_FOLDER = Path("uploads")  # TODO: move somewhere
@@ -82,11 +82,21 @@ def process():
 
 
 def make_the_printout(images, images_params):
+    # A4 at 300 DPI:
+    paper_width = 2480
+    paper_height = 3508
     bg_width = images_params["background_image"]["width"]
     bg_height = images_params["background_image"]["height"]
+    with Image.open(images["background_image"]) as background:
+        bg_width, bg_height = paper_width, round(
+            bg_height * (paper_width / bg_width)
+        )
+        background = background.resize((bg_width, bg_height))
+        background.save(images["background_image"])
+
     white_height = bg_height // 2
     image_height = bg_height * 2 + white_height + int(bg_height * 0.2)
-    printout = Image.new("RGBA", (bg_width, image_height), (255, 255, 255))
+    printout = Image.new("RGBA", (bg_width, image_height))
     with Image.open(images["background_image"]) as background:
         reversed_background = background.transpose(
             Image.Transpose.FLIP_TOP_BOTTOM
@@ -94,18 +104,61 @@ def make_the_printout(images, images_params):
         printout.paste(reversed_background, (0, 0))
         printout.paste(background, (0, bg_height))
         printout.paste(reversed_background, (0, 2 * bg_height + white_height))
+    with Image.open("dark_mask.png") as dark_mask:
+        dark_mask.resize((bg_width, bg_height))
+        printout.paste(dark_mask, (0, bg_height), dark_mask)
+
+    printout_draw = ImageDraw.Draw(printout)
     with Image.open(images["avatar_image"]) as avatar:
-        # TODO: deal with transparency?
+        # place the avatar image
         avatar_size = int(bg_width // 4)
         avatar = avatar.resize((avatar_size, avatar_size))
+        radius = 30
+        shift = 10
+        avatar = add_corners(avatar, radius)
+
+        left_top_x = bg_width - avatar_size // 8 - avatar_size
+        left_top_y = bg_height + avatar_size // 8
+
+        printout_draw.rounded_rectangle(
+            (
+                left_top_x - shift,
+                left_top_y - shift,
+                left_top_x + avatar_size + shift,
+                left_top_y + avatar_size + shift,
+            ),
+            # outline="black",
+            fill="orange",
+            radius=radius,
+            # width=10,
+        )
         printout.paste(
             avatar,
             (
-                bg_width - avatar_size // 10 - avatar_size,
-                int(bg_height + avatar_size // 10),
+                left_top_x,
+                left_top_y,
             ),
+            avatar,  # use alpha channel as a mask
         )
-
+    title_font = ImageFont.truetype("fonts/myriad-pro-semibold.ttf", 200)
+    subtitle_font = ImageFont.truetype("fonts/myriad-pro-semibold.ttf", 80)
+    stroke_width = 7
+    printout_draw.text(
+        (100, bg_height * 1.6),
+        "Имя персонажа",
+        (255, 255, 255),
+        font=title_font,
+        stroke_width=stroke_width,
+        stroke_fill="black",
+    )
+    printout_draw.text(
+        (100, bg_height * 1.77),
+        "человек эльф | разбойник".upper(),
+        (255, 255, 255),
+        font=subtitle_font,
+        stroke_width=stroke_width,
+        stroke_fill="black",
+    )
     filepath = f"{str(uuid.uuid4())}.png"
     filepath = UPLOAD_FOLDER / filepath
     printout.save(filepath, "PNG")
@@ -116,6 +169,21 @@ def clean_the_images(uploaded_images):
     # TODO: clean the images even if there were some errors during processing
     for img_name in uploaded_images:
         uploaded_images[img_name].unlink()  # remove the file
+
+
+# from https://stackoverflow.com/questions/11287402/how-to-round-corner-a-logo-without-white-backgroundtransparent-on-it-using-pi
+def add_corners(im, rad):
+    circle = Image.new("L", (rad * 2, rad * 2), 0)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
+    alpha = Image.new("L", im.size, 255)
+    w, h = im.size
+    alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
+    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
+    alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
+    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
+    im.putalpha(alpha)
+    return im
 
 
 if __name__ == "__main__":
